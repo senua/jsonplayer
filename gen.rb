984457bd037a33c4004b5e6244576a6e17868b8b
senua <etendren@gmail.com>
2014-08-21 00:45:43 +0400

    MINOR demonstration

--- gen.rb
+++ gen.rb
@@ -4,7 +4,7 @@ res = [];
 500.times do |i|
   spr = [];
   50.times do |id|
-    spr << {id: id, type: 'player', class: 'pl' + (id % 4).to_s, a: id*5 + i*(1 + id/10.0), x: 5 + id*9, y: 100 + Math.cos((i + id*5)/30.0)*50, info: id}
+    spr << {id: id, type: 'player', class: 'pl' + (id % 4).to_s, a: id*5 + i*(6.0 + 0.5*Math.sin((i+id)/5.0)), x: 5 + id*9, y: 100 + Math.cos((i + id*5)/30.0)*50, info: id}
   end
   res << {sprites: spr,  info: {a: {b:1, c:{a:[1,2,i,4]}}, b: 100} };
 end
